dd9a5896f094b75058c6b4db64f2eb7802d3166d
minh <minh@gmail.com>
2024-12-11 09:33:26 +0700

    completed

--- src/components/table/ManagingProduct.tsx
+++ src/components/table/ManagingProduct.tsx
@@ -100,7 +100,7 @@ function ManagingProduct() {
                     a number:</label>
                 <input type="number" id="number-input" aria-describedby="helper-text-explanation"
                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       placeholder="90210" required value={formdata?.number} name="number"
+                       placeholder="90210" required value={formdata?.number} name="number" min={1}
                        onChange={handleInputChange}/>
                 <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
                     an option</label>
