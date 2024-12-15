589eadbb196207ef78948b7d916762d27e7e9bb8
vandong <trinhdomg114@gmail.com>
2024-12-15 15:29:55 +0700

    tich hop xong

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -25,14 +25,11 @@ const Form = () => {
             console.log(authContextValue.login);
         }
     }
-<<<<<<< HEAD
     const validateEmail = (email:string) => {
         return email.match(
           /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
       };
-=======
->>>>>>> dong
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setRegister((prevState) => ({
@@ -44,7 +41,6 @@ const Form = () => {
 
         try {
             console.log("User data:", register);
-<<<<<<< HEAD
             if(validateEmail(register?.mail)){
                 console.log(import.meta.env.VITE_API_URL + "/v1/user/register");
                 const status = await CallUser().create(register);
@@ -55,14 +51,6 @@ const Form = () => {
             }
             console.log("loi email");
           
-=======
-            console.log(import.meta.env.VITE_API_URL + "/v1/user/register");
-            const status = await CallUser().create(register);
-            console.log("Response status:", status.status);
-            if (status.status == 200) {
-                navigate('/register/verify');
-            }
->>>>>>> dong
         } catch (error) {
             console.error("Error:", error);
         }
