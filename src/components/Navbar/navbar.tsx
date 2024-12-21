f61b75a916cff79295452ee65eb5bd8a36c54e64
Dat <Dangtandat43@gmail.com>
2024-12-21 08:48:55 +0700

    fix link navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -118,11 +118,6 @@ function NavBar() {
                                                 <TrashIcon className="size-4 fill-white/30"/>DashBoard</Link>
                                         </MenuItem>
                                         <MenuItem>
-                                            <Link to='/oderItems'
-                                                  className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'>
-                                                <TrashIcon className="size-4 fill-white/30"/>Shopping Cart</Link>
-                                        </MenuItem>
-                                        <MenuItem>
                                             <button
                                                 className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
                                                 <TrashIcon className="size-4 fill-white/30"/>
