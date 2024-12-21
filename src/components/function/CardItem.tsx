251955ca8a1225d8e810711d7ae53f2d812e90f2
Dat <Dangtandat43@gmail.com>
2024-12-21 08:48:04 +0700

    commit full

--- src/components/function/CardItem.tsx
+++ src/components/function/CardItem.tsx
@@ -35,7 +35,7 @@ function CardItem() {
         setItem((prevState) => {
             if (prevState) {
                 const updatedItems = prevState.filter((product) => product.product_id !== id);
-               // Cookies.set("item", JSON.stringify(updatedItems), { expires: 7 });
+                Cookies.set("item", JSON.stringify(updatedItems), { expires: 7 });
                 return updatedItems;
             }
             return prevState;
